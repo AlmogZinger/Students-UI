1d34ce3bb1f9caede33e41267b3591954cfff29e
almog <almog389@gmail.com>
2023-08-03 16:38:34 +0300

    almog 3/8

--- src/Student.h
+++ src/Student.h
@@ -6,25 +6,80 @@
 #define MYQTPROJECT_STUDENT_H
 #include <string>
 #include <list>
+#include  <vector>
 #include <QObject>
 #include <QString>
 #include <qqml.h>
 #include <QtGui>
 #include <iostream>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+
+inline const QByteArray HTTP_CONTENT_TYPE = "application/json";
+inline const QByteArray HTTP_ACCEPT = "application/graphql-response+json";
+
 class Student : public QAbstractListModel
 {
     Q_OBJECT
-
     Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
-
+    std::vector<Student> studentsGlobal;
 
 public:
-    Student(int id,QString name, int num_of_grade);
-    Student(QObject *parent = nullptr): QAbstractListModel(parent) {m_name = "almog" ;
-        auto a = 2;
-        m_grades.append(98);
-        m_grades.append(45);
+    Student(int id, QString name) {
+        m_id = id;
+        m_name = name;
     }
+
+    Student(const QJsonObject & data){
+        m_name= data["name"].toString();
+        m_id = data["id"].toInt();
+    };
+    Student(QObject *parent = nullptr): QAbstractListModel(parent)
+    {
+
+        QNetworkAccessManager * manager;
+        QNetworkRequest request;
+        manager = new QNetworkAccessManager();
+        QObject::connect(manager, &QNetworkAccessManager::finished,
+                         this, [=](QNetworkReply *reply) {
+                    if (reply->error()) {
+                        qDebug() << reply->errorString();
+                        return;
+                    }
+
+                    QByteArray answer = reply->readAll();
+                    QJsonDocument qJsonDocument = QJsonDocument::fromJson(answer);
+                    if (qJsonDocument.isNull()) {
+                        qDebug() << reply->errorString();
+                        return;
+                    }
+                    if (qJsonDocument.isArray()) {
+                        std::vector <Student*> students;
+
+                        for (const QJsonValue &jsonValue: qJsonDocument.array()) {
+                            if (jsonValue.isObject()) {
+                                QJsonObject data = jsonValue.toObject();
+                                students.push_back(Student(data));
+                            }
+                        }
+
+                    }
+                }
+        );
+        QJsonObject jsonObject
+                {
+                        {"query", "query{\n  students{\n   id\n   name\n   }\n}"}
+                };
+        auto doc = QJsonDocument(jsonObject).toJson();
+        request.setUrl(QUrl("http://127.0.0.1:8000/graphql/"));
+        request.setRawHeader("Accept", HTTP_ACCEPT);
+        request.setRawHeader("Content-Type", HTTP_CONTENT_TYPE);
+        manager->post(request, doc);
+
+    }
+
+
+
 
     void setName(const QString &na){
         if( na != m_name) {
@@ -80,7 +135,7 @@ signals:
     void nameChanged();
     //void gradesChanged();
 private:
-    int id;
+    int m_id;
     QString m_name;
     QVector<int> m_grades;
     //static void append_grades(QQmlListProperty<int> *list, int grade);
